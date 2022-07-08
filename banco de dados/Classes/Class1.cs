764d4abb166c8226f7a6f2149377f88508f98b8f
bernardi35235 <108948278+bernardi35235@users.noreply.github.com>
2022-07-08 13:50:22 -0300

    Initial commit

---
+++ banco de dados/Classes/Class1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace banco_de_dados.Classes
+{
+    public class Cliente
+    {
+        public string Nome;
+        public string Telefone;
+        public string CPF;
+        public void Gravar()
+        {
+            var clientes = Cliente.LerCLientes();
+            clientes.Add(this);
+            if (File.Exists(dBClinetes()))
+            {
+                string conteudo = "nome;telefone;cpf;\n";
+                foreach (Cliente c in clientes)
+                {
+                    conteudo += c.Nome;
+                }
+            }
+        }
+
+        private static string dBClinetes()
+        {
+            return @"C:\db\clientes.txt";
+        }
+
+
+        public static List<Cliente> LerCLientes()
+        {
+
+            var clientes = new List<Cliente>();
+            
+
+
+            if (File.Exists(dBClinetes()))
+            {
+
+                using (StreamReader arquivo = File.OpenText(dBClinetes()))
+                {
+                    string linha;
+                    int i = 0;
+                    while ((linha = arquivo.ReadLine()) != null)
+                    {
+                        i++;
+                        if (i == 1) continue;
+                        var clienteArquivo = linha.Split(';');
+
+                        var cliente = new Cliente { CPF = clienteArquivo[1], Nome = clienteArquivo[0], Telefone = clienteArquivo[2] };
+
+
+                        clientes.Add(cliente);
+                    }
+                }
+            }
+            return clientes;
+        }
+
+        
+
+    }
+}
+
---
+++ banco de dados/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using banco_de_dados;
+using banco_de_dados.Classes;
+
+namespace console_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+           var clientes = Cliente.LerCLientes();
+         
+
+            var cliente = new Cliente
+            {
+                Nome = "danilo",
+                CPF = "1232313123",
+                Telefone = "123861283"
+            };
+            var cliente2 = new Cliente
+            {
+                Nome = "roberto",
+                CPF = "1232313123",
+                Telefone = "123861283"
+            };
+
+            //foreach (Cliente c in clientes)
+            {
+                //Console.WriteLine(c.Nome);
+            }
+
+            
+            
+        }
+    }
+}
